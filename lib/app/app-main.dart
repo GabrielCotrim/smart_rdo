63d5a9ddb9a20b7dcfbe9b312253dfb1b544a21f
GabrielCotrim <gabrielcotrim@outlook.com>
2020-07-14 21:43:31 -0300

    master - Adiciona dependência do AppBloc à aplicação

--- lib/app/app-main.dart
+++ lib/app/app-main.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:smart_rdo/bloc/bloc.dart';
+import 'package:bloc/bloc.dart';
 import 'package:smart_rdo/screens/home_screen.dart';
-
 import 'app_theme.dart';
 
-class RdoApp extends StatelessWidget {
+class RdoApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => _RdoAppState();
+}
+
+class _RdoAppState extends State<RdoApp> {
+  AppBloc _appBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _appBloc = AppBloc();
+    Bloc.observer = SimpleBlocObserver();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: AppTheme.lightTheme,
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: BlocProvider(
+        create: (context) => _appBloc,
+        child: HomeScreen(),
+      ),
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate
@@ -23,51 +43,4 @@ class RdoApp extends StatelessWidget {
       ],
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pageOptions.elementAt(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.work),
-            title: Text('Agenda'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            title: Text('Áreas'),
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Color(0xff5676ef),
-        onTap: _onItemTapped,
-      ),
-    );
-  }
-
-  List<Widget> _pageOptions = <Widget>[
-    Home(),
-    Center(
-      child: Text('Another page'),
-    ),
-  ];
 }
